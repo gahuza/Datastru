f057d3f5b993cd6529339c5948d9476f33a7c5b4
gahuza <gahuzaj12@gmail.com>
2025-06-04 09:35:39 -0700

    well pushed task2

--- table1.js
+++ table1.js
@@ -1,11 +1,28 @@
+// const table1 = {
+//     "key1":"okello",
+//     "key2":"Jean de Dieu"
+// }
+
+// const table2 = new Map()
+// table2.set("name","Jean de Dieu");
+// table2.set("age","25");
+
+// console.log(table1["key1"]);
+// console.log(table2.get("age"));
+
+// Using Object
 const table1 = {
-    "key1":"okello",
-    "key2":"Jean de Dieu"
-}
-
-const table2 = new Map()
-table2.set("name","Jean de Dieu");
-table2.set("age","25");
-
-console.log(table1["key1"]);
-console.log(table2.get("age"));
+    "key1": "gahuzamiryango",
+    "key2": "value2"
+  };
+  console.log(table1["key1"]); // value1
+  
+  // Using Map
+  const table2 = new Map();
+  table2.set("okello", "60");
+  table2.set("age", 30);
+  //console.log(table2.get("name"),("age")); // John
+  
+  for (const [name, age] of table2) {
+    console.log("name:", name, "age:", age);
+  }
